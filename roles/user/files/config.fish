c7c6bb8f0b9c843602f0ced8758fa8bf929bfd73
Adrian Wallaschek <adrian@wallaschek.de>
2023-06-10 03:31:17 +0200

    Changed the config.fish to be copied instead of patched. In the initial run it does not exist, yet.

---
+++ roles/user/files/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+
+    # Initialize zoxide using proper fish-script-dialect.
+    #
+    zoxide init fish | source
+
+    # Commands to run in interactive sessions can go here
+end
